6c02a8599703fee8807c196eea42628e972ab25a
FoolyTD <trcariaga@utexas.edu>
2021-07-15 12:26:09 -0500

    updated leaderboards, added special perks, additional string validation, report button

--- src/questions/questions.js
+++ src/questions/questions.js
@@ -7,7 +7,9 @@ export default function Questions({ questions, restartQuiz, correctAnswer }) {
   const [count, setCount] = useState(0);
   const [timer, setTimer] = useState(0);
   const [answers, setAnswers] = useState([]);
-  // const [score, setScore] = useState(0);
+  const [skip, setSkip] = useState(1);
+  const [fiftyFifty, setFiftyFifty] = useState(null);
+  const [fiftyFiftyCount, setFiftyFiftyCount] = useState(1);
 
   useEffect(() => {
     setAnswers([
@@ -22,13 +24,6 @@ export default function Questions({ questions, restartQuiz, correctAnswer }) {
     } else {
       if (count >= 9) {
         history.push("/leaderboard");
-        // const alert = window.confirm(`You scored ${score} out of 10. Go home?`);
-        // if (alert) {
-        //   restartQuiz();
-        //   history.push("/home");
-        // } else {
-        //   return null;
-        // }
       }
       setTimer(0);
       setCount((currentCount) => currentCount+1); 
@@ -40,6 +35,7 @@ export default function Questions({ questions, restartQuiz, correctAnswer }) {
   const history = useHistory();
 
   const handleClick = ({ target }) => {
+    setFiftyFifty(false);
     if (count < 9) {
       if (target.value === questions[count].correct_answer && timer < 15) {
         correctAnswer();
@@ -48,13 +44,60 @@ export default function Questions({ questions, restartQuiz, correctAnswer }) {
       setTimer(0);
     } else {
       history.push("/leaderboard");
-      // const value = window.confirm(`You scored ${score} out of 10. Play again?`);
-      // if (value) {
-      //   restartQuiz();
-      //   history.push("/home");
-      // }
     }
   };
+
+  const handleSkip = () => {
+    setSkip((currentSkip)=>currentSkip-1);
+    correctAnswer();
+    setTimer(0);
+    setCount((currentCount)=>currentCount+1)
+  }
+
+  const handleReport = () => {
+    window.alert("We have flagged this question. Thank you for your feedback! Enjoy a free 50/50 on us.");
+    setFiftyFiftyCount((currentCount) => currentCount+1);
+  }
+
+  const displayFiftyFifty = () => {
+    setFiftyFiftyCount((currentCount)=>currentCount-1);
+    setFiftyFifty(!fiftyFifty);
+  }
+  const handleFiftyFifty = () => {
+    answers.sort((a, b) => a.localeCompare(b));
+    let numberOfAnswers = 0;
+    return answers.map((answer, index) => {
+      if (answer === questions[count].correct_answer) {
+        return (
+          <li key={`answer-${index}`}>
+            <button
+              className="category-buttons"
+              type="button"
+              value={answer}
+              onClick={handleClick}
+            >
+              {ValidateString(answer)}
+            </button>
+          </li>
+        );
+      } else if(numberOfAnswers === 0) {
+        numberOfAnswers++;
+        return (
+          <li key={`answer-${index}`}>
+            <button
+              className="category-buttons"
+              type="button"
+              value={answer}
+              onClick={handleClick}
+            >
+              {ValidateString(answer)}
+            </button>
+          </li>
+        );
+      }
+      return null;
+    })
+  }
 
   const listAnswers = () => {
     answers.sort((a, b) => a.localeCompare(b));
@@ -79,10 +122,15 @@ export default function Questions({ questions, restartQuiz, correctAnswer }) {
       <h1>{questions[count].category}</h1>
       <p>{questions && ValidateString(questions[count].question)}</p>
       <div>
-        <ul className="button-group">{answers && listAnswers()}</ul>
+        <ul className="button-group">{answers && fiftyFifty !== true ? listAnswers() : handleFiftyFifty()}</ul>
         <progress id="file" value={timer} max="15">Timer
         </progress>
       </div>
+      <div className="special-buttons">
+{skip > 0 ? <button className="active" onClick={handleSkip}>Free One</button> : null}
+{fiftyFiftyCount > 0 ? <button className="active" onClick={displayFiftyFifty}>50/50</button> : null}
+      </div>
+      <button className="danger" onClick={handleReport}>Report Question</button>
     </div>
   );
 }
--- src/questions/validateStrings.js
+++ src/questions/validateStrings.js
@@ -1,6 +1,6 @@
 export default function ValidateString(string) {
     let result = "";
-    if (string.match(/&quot/)) {
+    if (string.match(/&quot;/)) {
         result = string.replaceAll(/&quot;/g, '"');
     }
     if (string.match(/&#039/)) {
@@ -9,11 +9,32 @@ export default function ValidateString(string) {
     if (string.match(/&oacute/)) {
         result = string.replaceAll(/&oacute;/g, "ó");
     }
-    if (string.match(/&lrm/)) {
-        result = string.replaceAll(/&lrm/g, "-");
+    if (string.match(/&lrm;/)) {
+        result = string.replaceAll(/&lrm;/g, "-");
     }
-    if (string.match(/&rsquo/)) {
+    if (string.match(/&rsquo;/)) {
         result = string.replaceAll(/&rsquo;/g, "'");
+    }
+    if (string.match(/&amp;/)) {
+        result = string.replaceAll(/&amp;/g, "&");
+    }
+    if (string.match(/&shy;/)) {
+        result = string.replaceAll(/&shy;/g, "-");
+    }
+    if (string.match(/&Eacute;/)) {
+        result = string.replaceAll(/&Eacute;/g, "É");
+    }
+    if (string.match(/&ldquo;/)) {
+        result = string.replaceAll(/&ldquo;/g, '"');
+    }
+    if (string.match(/&rdquo;/)) {
+        result = string.replaceAll(/&rdquo;/g, '"');
+    }
+    if (string.match(/&#039;/)) {
+        result = string.replaceAll(/&#039;/g, "'");
+    }
+    if (string.match(/&hellip;/)) {
+        result = string.replaceAll(/&hellip;/g, "___");
     }
     return result ? result : string;
 }
